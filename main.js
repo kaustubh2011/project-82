c8fb34fe2cc5c1e5d6a59f01399723410a5ac5a5
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2021-06-12 13:46:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var mouseevent="empty";
+var last_position_of_x, last_position_of_y;
+canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+color="black";
+width=1;
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e)
+{
+    color=document.getElementById("color").value;
+    width=document.getElementById("width").value;
+    mouseevent="mousedown";
+}
+canvas.addEventListener("mousemove", my_mousemove);
+function my_mousemove(e)
+{
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft; 
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+    if(mouseevent=="mousedown")
+    {
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth=width;
+        ctx.moveTo(last_position_of_x,last_position_of_y);
+        ctx.lineTo(current_position_of_mouse_x,current_position_of_mouse_y);
+        ctx.stroke();
+    }
+    last_position_of_x=current_position_of_mouse_x;
+    last_position_of_y=current_position_of_mouse_y;
+}
+canvas.addEventListener("mouseup", my_mouseup);
+function my_mouseup(e)
+{
+    mouseevent="mouseup";
+}
+canvas.addEventListener("mouseleave", my_mouseleave);
+function my_mouseleave(e)
+{
+    mouseevent="mouseleave";
+}
+function clearArea()
+ { ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); }
